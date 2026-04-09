f3d9fa9907f6709893e395b48e2aced7aafadd0a
Nvann07 <nrizwardi@gmail.com>
2026-04-09 19:15:11 +0700

    menambahakan event clik datagridview untuk menampilkan data ke form

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -258,6 +258,22 @@ namespace CRUDMahasiswaADO
             }
         }
 
-       
+        // ===============================
+        // DATA GRID VIEW CLICK
+        // ===============================
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                txtNIM.Text = row.Cells["NIM"].Value.ToString();
+                txtNama.Text = row.Cells["Nama"].Value.ToString();
+                cmbJK.Text = row.Cells["JenisKelamin"].Value.ToString();
+                dtpTanggalLahir.Value = Convert.ToDateTime(row.Cells["TanggalLahir"].Value);
+                txtAlamat.Text = row.Cells["Alamat"].Value.ToString();
+                txtKodeProdi.Text = row.Cells["KodeProdi"].Value.ToString();
+            }
+        }
 
         
