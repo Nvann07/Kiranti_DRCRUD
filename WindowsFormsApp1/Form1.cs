c41a294b8d4311d72b254a3af367d61a946c976d
Nvann07 <nrizwardi@gmail.com>
2026-04-09 19:10:32 +0700

    menambhakan fitur load data  mahasiswa ke datagridview

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -39,4 +39,59 @@ namespace CRUDMahasiswaADO
             }
         }
 
+        // ===============================
+        // BUTTON CONNECT
+        // ===============================
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectDatabase();
+        }
+
+        // ===============================
+        // LOAD DATA MAHASISWA
+        // ===============================
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                dataGridView1.Rows.Clear();
+
+                if (dataGridView1.Columns.Count == 0)
+                {
+                    dataGridView1.Columns.Add("NIM", "NIM");
+                    dataGridView1.Columns.Add("Nama", "Nama");
+                    dataGridView1.Columns.Add("JenisKelamin", "Jenis Kelamin");
+                    dataGridView1.Columns.Add("TanggalLahir", "Tanggal Lahir");
+                    dataGridView1.Columns.Add("Alamat", "Alamat");
+                    dataGridView1.Columns.Add("KodeProdi", "Kode Prodi");
+                }
+
+                string query = "SELECT * FROM Mahasiswa";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["NIM"].ToString(),
+                        reader["Nama"].ToString(),
+                        reader["JenisKelamin"].ToString(),
+                        Convert.ToDateTime(reader["TanggalLahir"]).ToShortDateString(),
+                        reader["Alamat"].ToString(),
+                        reader["KodeProdi"].ToString()
+                    );
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan data : " + ex.Message);
+            }
+        }
+
       
